3560507a816a2f87474095e181b418ba693136c4
ireh1214 <pir@driven.co.kr>
2024-10-11 15:58:00 +0900

    css

--- src/app/layout/ad/custInfo/page.tsx
+++ src/app/layout/ad/custInfo/page.tsx
@@ -6,8 +6,30 @@ import { PiMicrosoftExcelLogoFill } from "react-icons/pi";
 import { IoIosArrowDown, IoIosClose } from "react-icons/io";
 import { MdOutlineCalendarToday } from "react-icons/md";
 import CommonDataGrid from "@/app/components/CommonDataGrid";
+import { useState } from "react";
 
 export default function page (){
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [filterClass, setFilterClass] = useState('');
+
+ 
+    // 클릭 핸들러
+    const handleClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+      const clickedElement = e.currentTarget;
+  
+      // 조건에 따라 클래스 설정
+      if (clickedElement.classList.contains('text')) {
+        setFilterClass('text');
+      } else if (clickedElement.classList.contains('cal')) {
+        setFilterClass('calendar');
+      } else if (clickedElement.classList.contains('check')) {
+        setFilterClass('check');
+    } else if (clickedElement.classList.contains('toggle')) {
+        setFilterClass('toggle');
+      } else {
+        setFilterClass(''); // 아무 클래스도 없을 때
+      }
+    };
     return (
 <CommonLayout>
 <div className="custInfo_wrap">
@@ -21,20 +43,20 @@ export default function page (){
 <div className="filter">
 {/* <button type="button" className="type1">탭 표시<IoIosArrowDown color="#fff" /></button> */}
 
-<div className="search_filter">
-<button type="button" className="type2">검색필터<IoIosArrowDown color="#fff" /></button>
+<div className={`search_filter ${isFilterOpen ? 'on' : ''}`} >
+<button type="button" className="type2"  onClick={() => setIsFilterOpen(!isFilterOpen)} >검색필터<IoIosArrowDown color="#fff" /></button>
 <div className="output">
     <ul className="list">
-        <li>이름</li>
-        <li>날짜</li>
-        <li>실비</li>
-        <li>품질</li>
+        <li className="text" onClick={handleClick}>이름</li>
+        <li className="cal" onClick={handleClick}>날짜</li>
+        <li className="check" onClick={handleClick}>실비</li>
+        <li className="toggle" onClick={handleClick}>품질</li>
         <li>품질변경사유</li>
         <li>상태</li>
         <li>상태변경사유</li>
         <li>사용여부</li>
     </ul>
-    <div className="filter_box check">
+    <div className={`filter_box ${filterClass}`}>
      {/* calendar type */}
      <div className="calendar_type">
 <p>
@@ -63,6 +85,11 @@ export default function page (){
     <li> <input type="checkbox" name="" id="3" /> <label htmlFor="3">체크리스트1</label></li>
     <li> <input type="checkbox" name="" id="4" /> <label htmlFor="4">체크리스트1</label></li>
 </ul>
+</div>
+
+{/* text_type */}
+<div className="text_type">
+<input type="text" placeholder="검색어를 입력하세요" />
 </div>
     </div>
 </div>
