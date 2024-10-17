d454285c99cbc1fe52785eaeea0d45b7ce4d55b0
ireh1214 <pir@driven.co.kr>
2024-10-17 18:29:45 +0900

    css

--- src/app/layout/[companyId]/login/page.tsx
+++ src/app/layout/[companyId]/login/page.tsx
@@ -77,8 +77,20 @@ const Login: NextPage<LoginProps> = ({ params }) => {
             <em className="comp">{companyName}</em>
             <p>데이터몬에 오신것을 환영합니다.</p>
             <div className="form_cont">
-                <input type="text" value={idValue} onChange={(event)=>{setId(event.target.value)}} placeholder="아이디"/>
-                <input type="password" value={pwValue} onChange={(event)=>{setPw(event.target.value)}} placeholder="비밀번호"/>
+                <input type="text"  value={idValue} onChange={(event)=>{setId(event.target.value)}} 
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter") {
+                                loginEffect();
+                            }
+                        }}
+                        placeholder="아이디"/>
+                <input type="password" value={pwValue} onChange={(event)=>{setPw(event.target.value)}} 
+                        onKeyDown={(event) => {
+                            if (event.key === "Enter") {
+                                loginEffect();
+                            }
+                        }}
+                placeholder="비밀번호"/>
                 {/* <div>
                     <div className="check_box">
                         <input id="id" type="checkbox"/> <label htmlFor="id">아이디저장</label>
@@ -86,7 +98,7 @@ const Login: NextPage<LoginProps> = ({ params }) => {
                     <p><Link href="">아이디/비밀번호 찾기</Link></p>
                 </div> */}
                 {/* 추후 기능 개발 시 style 삭제 요망 */}
-                <button type="button" style={{marginTop: "24px"}} className="login" onClick={loginEffect}>로그인</button>
+                <button type="submit" style={{marginTop: "24px"}} className="login" onClick={loginEffect}>로그인</button>
                 {/* <button>회원가입</button> */}
             </div>
         </div>
