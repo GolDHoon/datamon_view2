de53d121f4a3f9e0df5e286c510b6c62a3879ff6
손철훈 <sch9208@gmail.com>
2024-11-11 10:49:43 +0900

    운영버그 수정

--- src/app/layout/[companyId]/login/page.tsx
+++ src/app/layout/[companyId]/login/page.tsx
@@ -57,7 +57,7 @@ const Login : NextPage<LoginProps> = ({params}) => {
             restApi('post','/sign/login',{
                 userId: idValue,password: pwValue,companyIdx: idxValue
             }).then(response => {
-                console.log(reponse)
+                console.log(response)
                 // @ts-ignore
                 if (response.status === 200) {
                     setSession('userType',response.data.userType)
